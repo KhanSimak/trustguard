e39629eef5ee757c1bfc3c6222183945fd561bb3
Simak <your-email@example.com>
2026-08-06 04:02:30 +0530

    updated config

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:8000",
         "http://localhost:8000"
+        "https://trustguard-wfry.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
