fa75660e7acba121207168a536187acfb0fbab97
Simak <your-email@example.com>
2026-08-06 04:17:24 +0530

    Allow Vercel frontend

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "https://trustguard-wfry.vercel.app/"
+        "https://trustguard-qz68.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
