ce8c11031f8113fad6454c79f9e7f76c5c5587c8
Simak <your-email@example.com>
2026-08-05 17:21:07 +0530

    Download ONNX model from Hugging Face

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
+from huggingface_hub import snapshot_download
+
+MODEL_PATH = snapshot_download(
+    repo_id="simak31/trustguard-onxx"
+)
+
 MAX_LENGTH = 256
-
 THRESHOLD = 0.35
-
-MODEL_PATH = "./trustguard_onnx"
